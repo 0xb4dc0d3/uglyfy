5329bbdd82c1e4e1da58f54f399912fe66634942
Diego Collao <dcollao@icloud.com>
2017-10-22 08:49:47 -0300

    it works!

--- src/App.vue
+++ src/App.vue
@@ -9,23 +9,24 @@
     </header>
   <b-container>
     <b-row>
-      <b-col>
+      <b-col v-for="item in items">
+
+        <!--
         <b-card header="Remington 1911" header-text-variant="white" header-tag="header" style="max-width:20rem;" header-bg-variant="success" header-border-variant="dark" footer="<p>Precio: $ <b>560.000</b></p>" footer-tag="footer" img-fluid footer-bg-variant="light"
           footer-border-variant="dark">
           <b-carousel id="carousel" controls indicators background="white" :interval="2000" v-model="slide" @sliding-start="onSlideStart" @sliding-end="onSlideEnd">
             <b-carousel-slide img-src="src/assets/1911r1c-1.png"></b-carousel-slide>
             <b-carousel-slide img-src="src/assets/1911r1c-2.jpg"></b-carousel-slide>
             <b-carousel-slide img-src="src/assets/1911r1c-3.jpg"> </b-carousel-slide>
-          </b-carousel>
+          </b-carousel> -->
           <div>
             <br>
             <div class="text-center">
               <b-btn v-b-toggle.collapse1 variant="success">Ver más</b-btn>
               <b-collapse id="collapse1" class="text-justify">
                 <br>
-                <p>Modelo: <b>R1 Carry</b></p>
-                <p>The proven accuracy of the Model 1911 completely de-horned for everyday carry. It packs a checkered front strap for a more positive grip and Novak® tritium front night sight so your aim stays sure in moments of duress. Also features an
-                  ambidextrous safety, a machined skeletonized aluminum trigger and cocobolo grips.</p>
+                <p>Modelo: <b>{{item.model}}</b></p>
+                <p>{{item.description}}</p>
               </b-collapse>
             </div>
           </div>
@@ -36,25 +37,20 @@
 </div>
 </template>
 
-
-
 <script>
 export default {
   name: 'app',
-  data: {
-    showCollapse: true,
-    slide: 0,
-    sliding: null
-  },
-  methods: {
-    onSlideStart(slide) {
-      this.sliding = true;
-    },
-    onSlideEnd(slide) {
-      this.sliding = false;
-    }
-  }
-}
+  data() {
+  return {
+    items: [
+      { invId: 1, model: '1911 R1 Carry', description: 'The proven accuracy of the Model 1911 completely de-horned for everyday carry. It packs a checkered front strap for a more positive grip and Novak® tritium front night sight so your aim stays sure in moments of duress. Also features an ambidextrous safety, a machined skeletonized aluminum trigger and cocobolo grips.' },
+      { invId: 2, model: 'An Item', description: 'ioioioi' },
+      { invId: 3, model: 'An Item', description: 'klklklk' },
+      { invId: 4, model: 'An Item', description: 'asasasa' },
+    ],
+  };
+},
+};
 </script>
 
 
